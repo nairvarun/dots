d27e0fc1cd5b3deeaf5cd33f2fc2633fabc0cb10
varun <10447555-nairvarun@users.noreply.gitlab.com>
2021-12-31 12:38:18 +0530

    added lazy loading

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,3 +1,4 @@
+-- installs packer if not already installed
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -5,33 +6,36 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd [[packadd packer.nvim]]
 end
 
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
+
+-- calls require("packer"). if it doesnt work, it'll notify and not throw a big error. if it works, it'll return require("packer") as packer.
+local ok, packer = pcall(require, "packer")
+if not ok then
 	vim.notify('something went worng with packer')
 end
 
 
 return packer.startup({function(use)
-	-- packer: (so that it manages itself)
-	use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim' -- so that packer manages itself
 	
-	-- other:
-	-- use 'nvim-lua/popup.nvim'
-	-- use 'nvim-lua/plenary.nvim'
-	use 'wfxr/minimap.vim'
-		-- requires 'https://github.com/wfxr/code-minimap'
-		-- cargo install --locked code-minimap
+	-- use 'nvim-lua/popup.nvim'		-- required by other plugins
+	-- use 'nvim-lua/plenary.nvim'		-- required by other plugins
+	use {
+		'wfxr/minimap.vim', -- requires 'https://github.com/wfxr/code-minimap' (cargo install --locked code-minimap)
+		opt = true,
+		cmd = 'MinimapToggle'
+	}
 	use {
 		'iamcco/markdown-preview.nvim', 
-		run = 'cd app && yarn install'
+		run = 'cd app && yarn install',
+		opt = true,
+		ft = 'markdown'
 	}
 
-	-- colors:
 	use 'folke/tokyonight.nvim'
 	-- use 'morhetz/gruvbox'
 	-- use 'lifepillar/vim-solarized8'
 	-- use 'arcticicestudio/nord-vim'
-	-- use 'sainnhe/everforest'
+	-- use 'sainnhe/everforest'}
 	-- use 'joshdick/onedark.vim'
 	-- use 'plan9-for-vimspace/acme-colors'
 	-- use 'AlessandroYorba/Alduin'
@@ -42,10 +46,12 @@ return packer.startup({function(use)
 	end
 end,
 config = {
-	display = {
-		open_fn = function()
-			return require('packer.util').float({ border = 'single' })
-		end
+	-- display = {
+	-- 	open_fn = require('packer.util').float
+	-- },
+	profile = {
+		enable = true,
+		threshold = 0
 	}
 }})
 
