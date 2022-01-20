28b6c173b6fe4d16082628f18f61150eb8e62ba0
varun <10447555-nairvarun@users.noreply.gitlab.com>
2022-01-20 22:02:47 +0530

    added winsay

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,95 +1,139 @@
-# todo{{{
-	# organize pls
-#}}}
-
-# custom prompt{{{
-#{{{
-# default:
-    # "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
-
-# ref:
-    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts?view=powershell-7.2
-    # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-host?view=powershell-7.2 
-    # https://stackoverflow.com/questions/61520315/powershell-get-current-script-directory-name-only
-	# https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git
-#}}}
+# custom prompt
 function prompt {
+    # default:
+        # "PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
 
-	$principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
-	$adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+    # ref:
+        # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_prompts?view=powershell-7.2
+        # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/write-host?view=powershell-7.2 
+        # https://stackoverflow.com/questions/61520315/powershell-get-current-script-directory-name-only
+        # https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git
 
-	if($principal.IsInRole($adminRole)) {
-		Write-Host ("[ADMIN] ") -NoNewLine -ForegroundColor Blue
-	} 
+    $principal = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+    if($principal.IsInRole($adminRole)) {
+        Write-Host ("[ADMIN] ") -NoNewLine -ForegroundColor Blue
+    } 
 
     Write-Host ("$(Split-Path -Path (Get-Location) -Leaf)") -NoNewLine -ForegroundColor Green
 
-	if(git rev-parse --is-inside-work-tree) {
-		Write-Host (" [$(git rev-parse --abbrev-ref HEAD)]") -NoNewLine -ForegroundColor Cyan
-	} 
+    if(git rev-parse --is-inside-work-tree) {
+        Write-Host (" [$(git rev-parse --abbrev-ref HEAD)]") -NoNewLine -ForegroundColor Cyan
+    } 
 
-	if ("$(git status -s)") {
-		Write-Host ("*") -NoNewLine -ForegroundColor Red
-	}
+    if ("$(git status -s)") {
+        Write-Host ("*") -NoNewLine -ForegroundColor Red
+    }
 
     Write-Host (" ->") -NoNewLine -ForegroundColor Green
-	return " "
+    return " "
 }
-#}}}
 
-# open ms teams{{{
+# open ms teams
 function msto {
     C:\Users\nairv\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe"
 }
-#}}}
-# close ms teams and all its child processes{{{
+
+# close ms teams and all its child processes
 function mstc {
     TASKKILL /F /IM Teams.exe /T
 }
-#}}}
 
-# open brave browser (to specified website or search){{{
+# open brave browser (to specified website or search)
 function brv {
 
-	param(
-		$Query,
-		$QueryType=" "
-	)	
+    param(
+        $Query,
+        $QueryType=" "
+    )	
 
     if ($QueryType -eq "?") {
-		
+        
         start brave google.com/search?q=$($Query.Replace(' ', '+'))
-		} else {
+        } else {
         start brave $Query
     }
 }
-#}}}
 
-# get into a docker container's shell{{{
-# 
-# ref:
-# 	- https://powershell.one/powershell-internals/attributes/auto-completion
-#
+# get into a docker container's shell
 function dl {
-	
-	param (
-		[ArgumentCompleter({ docker ps --format "{{.Names}}" })]$ContainerName	
-	)
+    # ref:
+    # 	- https://powershell.one/powershell-internals/attributes/auto-completion
+    
+    param (
+        [ArgumentCompleter({ docker ps --format "{{.Names}}" })]$ContainerName	
+    )
 
-	docker exec -it $ContainerName	env TERM=xterm-256color bash -l
+    docker exec -it $ContainerName	env TERM=xterm-256color bash -l
 }
-#}}}
 
-# open all files of a certain file type{{{
+# open all files of a certain file type
 function oft {
 
-	param (
-		[ArgumentCompleter({ (Get-Childitem | Select-Object Extension -Unique).Extension })] $FileType
-	)
-	$Files = @(Get-Childitem *$FileType)
-	for($i = 0; $i -lt $Files.Count; $i++) {
-		& $Files[$i]
-	}
+    param (
+        [ArgumentCompleter({ (Get-Childitem | Select-Object Extension -Unique).Extension })] $FileType
+    )
+    $Files = @(Get-Childitem *$FileType)
+    for($i = 0; $i -lt $Files.Count; $i++) {
+        & $Files[$i]
+    }
 }
-#}}}
 
+# make cowsay type thing
+function winsay {
+
+    param ($text)
+
+    $text_bubble = ("`r`n " + "_" * ($text.length + 4) + "`r`n<  $text  >`r`n" + " " + "-" * ($text.length + 4))
+
+    $text_output =
+    "$text_bubble 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+    "
+    Write-Host ($text_output) -ForegroundColor Blue
+
+    #region other ascii
+    # ▄██████████████▄▐█▄▄▄▄█▌
+    # ██████▌▄▌▄▐▐▌███▌▀▀██▀▀
+    # ████▄█▌▄▌▄▐▐▌▀███▄▄█▌
+    # ▄▄▄▄▄██████████████▀
+
+    #        _                        
+    #        \`*-.                    
+    #         )  _`-.                 
+    #        .  : `. .                
+    #        : _   '  \               
+    #        ; *` _.   `*-._          
+    #        `-.-'          `-.       
+    #          ;       `       `.     
+    #          :.       .        \    
+    #          . \  .   :   .-'   .   
+    #          '  `+.;  ;  '      :   
+    #          :  '  |    ;       ;-. 
+    #          ; '   : :`-:     _.`* ;
+    # [bug] .*' /  .*' ; .*`- +'  `*' 
+    #       `*-*   `*-*  `*-*'
+
+    # Whale by Riitta Rasimus
+    #        .
+    #       ":"
+    #     ___:____     |"\/"|
+    #   ,'        `.    \  /
+    #   |  O        \___/  |
+    # ~^~^~^~^~^~^~^~^~^~^~^~^~
+
+    #  _________ 
+    # < default >
+    #  --------- 
+    #         \   ^__^
+    #          \  (oo)\_______
+    #             (__)\       )\/\
+    #                 ||----w |
+    #                 ||     ||
+    #endregion other
+}
